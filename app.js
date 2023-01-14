30b1c3d49850da0c558f492cd0e3a99c27031ee9
facu <facu11ja@gmail.com>
2023-01-14 19:36:11 -0300

    ni idea

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require ('express');
 const app = express();
 const router = express.router;
-
-
+const path = require('node:path');
+app.set('views', path.join(__dirname,'/views'));
 app.get('/',(req,res)=> res.send ('hello world'));
 
 app.listen(process.env.PORT||3000);
